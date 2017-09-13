7595b6e1dedf1a9b1efef2c965db07ae6d0dda1e
Justin Kim <jjkim928@gmail.com>
2017-09-13 18:01:28 -0400

    added printText method

--- src/com/company/Notification.java
+++ src/com/company/Notification.java
@@ -6,12 +6,13 @@ public class Notification {
     private LocalDateTime createdAt;
     private String subject;
     private String body;
-    protected String status = "Good";
+    protected String status;
 
     public Notification(String subject, String body) {
         this.subject = subject;
         this.body = body;
         this.createdAt = LocalDateTime.now();
+        this.status = "Good";
     }
 
     public LocalDateTime getCreatedAt() {
@@ -24,6 +25,15 @@ public class Notification {
 
     public String getBody() {
         return body;
+    }
+
+    public void showStatus() {
+        System.out.println(status);
+    }
+
+    protected String printText(String text) {
+        System.out.println(text);
+        return text;
     }
 
     public void transport() throws NoTransportException {
