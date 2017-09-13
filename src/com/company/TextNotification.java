da5de789a9e522e0c49f839445e2d37c1dd4a5f6
Justin Kim <jjkim928@gmail.com>
2017-09-13 17:20:40 -0400

    added file

---
+++ src/com/company/TextNotification.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class TextNotification extends Notification {
+    private String recipient;
+    private String smsProvider;
+
+    public TextNotification(String subject, String body, String recipient, String smsProvider) {
+        super(subject, body);
+        this.recipient = recipient;
+        this.smsProvider = smsProvider;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public String getSmsProvider() {
+        return smsProvider;
+    }
+
+    @Override
+    public void transport() throws NoTransportException {
+        // super.transport(); do I need this?
+        System.out.println(super.getCreatedAt());
+        System.out.println(super.getSubject());
+        System.out.println(super.getBody());
+        System.out.println(getRecipient());
+        System.out.println(getSmsProvider());
+    }
+}
