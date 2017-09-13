16800bcba3127e3a345aa903d878ad68f4be9b60
Justin Kim <jjkim928@gmail.com>
2017-09-13 17:09:39 -0400

    skeleton structure

--- src/com/company/Email.java
+++ src/com/company/Email.java
@@ -1,4 +1,22 @@
 package com.company;
 
-public class Email {
+public class Email extends Notification {
+    private String recipient;
+    private String smtpProvider;
+
+    // right now compiler is complaining there's no default class because I haven't set the super method yet
+
+    public Email(String subject, String body, String recipient, String smtpProvider) {
+        super(subject, body);
+        this.recipient = recipient;
+        this.smtpProvider = smtpProvider;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public String getSmtpProvider() {
+        return smtpProvider;
+    }
 }
