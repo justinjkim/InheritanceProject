6d5d708ae611db98f473c9e226b7976d0caa19e1
Justin Kim <jjkim928@gmail.com>
2017-09-13 17:26:33 -0400

    created instances of email and text notification

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import javax.xml.soap.Text;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        EmailNotification emailNotification = new EmailNotification("Hello", "It's great to hear from you, George.", "Justin", "SendGrid");
+        TextNotification textNotification = new TextNotification("Hey", "Dinner at 8?", "Deanna", "Verizon");
+        emailNotification.transport();
+        textNotification.transport();
     }
 }
