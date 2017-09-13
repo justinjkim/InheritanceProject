489314cf5ed4e016ff70aafe2fb46aec32aefe89
Justin Kim <jjkim928@gmail.com>
2017-09-13 17:21:52 -0400

    fixed typo in name

--- src/com/company/EmailNotification.java
+++ src/com/company/EmailNotification.java
@@ -1,12 +1,12 @@
 package com.company;
 
-public class Email extends Notification {
+public class EmailNotification extends Notification {
     private String recipient;
     private String smtpProvider;
 
     // right now compiler is complaining there's no default class because I haven't set the super method yet
 
-    public Email(String subject, String body, String recipient, String smtpProvider) {
+    public EmailNotification(String subject, String body, String recipient, String smtpProvider) {
         super(subject, body);
         this.recipient = recipient;
         this.smtpProvider = smtpProvider;
